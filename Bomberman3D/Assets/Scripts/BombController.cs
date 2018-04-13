c3f7c541bdeba01c42e36dafb3fda642c1883e08
victorMunteanu <victor.munteanu0201@stud.acs.upb.ro>
2018-04-13 10:53:54 +0300

    fixed bomb-player collisions

--- Bomberman3D/Assets/Scripts/BombController.cs
+++ Bomberman3D/Assets/Scripts/BombController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class BombController : MonoBehaviour {
     public int bombRange;
 
+    // SerializeField means that Unity will create a Field in the interface so you can modify the attribute
+    // click on Bomb prefab and you will see the field under "BombController" Component
     [SerializeField]
     private float explosionDelay;
 
+    private BoxCollider bc;
+
     // Use this for initialization
     void Start () {
+        bc = GetComponent<BoxCollider>();
+        bc.isTrigger = true;
+
+        // in explosionDelay seconds, the Explode function will be called
         Invoke("Explode", explosionDelay);
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    private void OnTriggerExit(Collider other)
+    {
+        // if the Player exits the Trigger, the Trigger becomes a Collider
+        if (other.gameObject.tag.Equals("Player"))
+            bc.isTrigger = false;
+    }
 
     void Explode()
     {
+        // built in function .. simillar to free(x);
         Destroy(gameObject);
     }
 }
